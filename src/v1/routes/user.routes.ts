5ebb3dc6b07bdeb4a96636f14b5c736dbe4767d9
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2023-04-01 22:36:20 +0600

    verify JWT

--- src/v1/routes/user.routes.ts
+++ src/v1/routes/user.routes.ts
@@ -1,19 +1,17 @@
 import express from 'express';
 import UserController from '../../controllers/user.controller';
-
+import verifyJWT from '../../middlewares/verifyJWT';
 
 export const router = express.Router();
 
-
-
 // GET: Get all users
-router.route("/users").get(UserController.getAll);
+router.route('/users').get(UserController.getAll);
 
 // GET: Get user by id
-router.route("/users/:userId").get(UserController.getById);
+router.route('/users/:userId').get(UserController.getById);
 
 // PUT: Update user by id
-router.route("/users/:userId").put(UserController.update);
+router.route('/users/:userId').put(verifyJWT, UserController.update);
 
 // DELETE: Delete user by id
-router.route("/users/:userId").delete(UserController.delete);
+router.route('/users/:userId').delete(verifyJWT, UserController.delete);
